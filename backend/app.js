40157321b44337292797c76fb7f8322db9383563
Dmitry Konavalenak <konavalenak@mail.ru>
2023-11-05 00:48:47 +0300

    temp crash off

--- backend/app.js
+++ backend/app.js
@@ -33,11 +33,11 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(requestLogger);
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
