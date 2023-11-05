fbab8bb812a791645dfa9f1d6f38223faea8fd0e
Dmitry Konavalenak <konavalenak@mail.ru>
2023-11-05 21:49:04 +0300

    newCard fix

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -76,13 +76,11 @@ export default function App() {
 
   function handleCardLike(card) {
     const isLiked = card.likes.some((i) => i === currentUser._id);
-    console.log(currentUser._id)
-    console.log(currentUser._id)
     api
       .changeLikeCardStatus(card._id, isLiked)
       .then((newCard) => {
         setCards((state) =>
-          state.map((c) => (c._id === card._id ? newCard : c))
+          state.map((c) => (c._id === card._id ? newCard.data : c))
         );
       })
       .catch((err) => {
